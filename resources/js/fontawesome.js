f2405092f98f1678014e55e795d7ddafff3ae8a3
Yanik Kumar <believemasters@gmail.com>
2020-06-04 15:21:16 +0530

    Added voting controls to the answers and questions in details

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,7 @@
+import fontawesome from "@fortawesome/fontawesome";
+import faCaretUp from "@fortawesome/fontawesome-free-solid/faCaretUp.js";
+import faCaretDown from "@fortawesome/fontawesome-free-solid/faCaretDown.js";
+import faStar from "@fortawesome/fontawesome-free-solid/faStar.js";
+import faCheck from "@fortawesome/fontawesome-free-solid/faCheck.js";
+
+fontawesome.library.add(faCaretUp, faCaretDown, faCheck, faStar);
