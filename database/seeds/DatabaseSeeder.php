26033f43a747638cc744b57ff1b10d654cc89e65
Yanik Kumar <believemasters@gmail.com>
2020-06-05 12:13:02 +0530

    Implemented many to many relationships and make favorites button working

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,15 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // To make fake data for users and questions insert method create records into database and make methods generate objects and store in memory
-        factory(User::class, 3)->create()->each(function ($u) {
-            $u->questions()
-                ->saveMany(
-                    factory(Question::class, rand(1, 5))->make()
-                )
-                ->each(function ($q) {
-                    $q->answers()
-                        ->saveMany(factory(Answer::class, rand(1, 5))->make());
-                });
-        });
+        $this->call([
+            UsersQuestionsAnswersTableSeeder::class,
+            FavoritesTableSeeder::class,
+        ]);
     }
 }
