83b830cadc708311764bd8d33b2d81a511d7fb04
Yanik Kumar <believemasters@gmail.com>
2020-06-17 23:37:03 +0530

    Add markdown to answer component

--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -20,7 +20,7 @@
                     <div class="media">
                         <div class="media-body">
                             <div class="form-group">
-                                <m-editor :body="body">
+                                <m-editor :body="body" :name="uniqueName">
                                     <textarea
                                         rows="10"
                                         v-model="body"
@@ -106,14 +106,10 @@
 </template>
 
 <script>
-import Vote from "./Vote.vue";
-import UserInfo from "./UserInfo.vue";
-import MEditor from "./MEditor.vue";
 import modification from "../mixins/modification";
 export default {
     props: ["question"],
     mixins: [modification],
-    components: { Vote, UserInfo, MEditor },
     data() {
         return {
             title: this.question.title,
@@ -129,6 +125,9 @@ export default {
         },
         endpoint() {
             return `/questions/${this.id}`;
+        },
+        uniqueName() {
+            return `question-${this.id}`;
         }
     },
     methods: {
